e30904f95b93960ed697876e8e472802af2fa762
lisandrofernando <lisandrusfernandus@gmail.com>
2022-06-07 13:23:57 -0500

    Adding Host on port and server

--- app.js
+++ app.js
@@ -24,4 +24,4 @@ app.get('',(req,res) => {
 
 
 /* listen on port 5000 */
-app.listen(port,host,()=> console.info(`Listening on port ${port}`));
+app.listen(process.env.Port, '0.0.0.0',()=> console.info(`Listening on port ${port}`));
